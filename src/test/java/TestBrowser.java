0535e9c2e00be80bb2705bd08137647f9226e72d
Olawale <olapetanwale@outlook.com>
2023-12-31 17:02:21 +0100

    Second commit Dec 31st

--- src/test/java/TestBrowser.java
+++ src/test/java/TestBrowser.java
@@ -1,7 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 public class TestBrowser {
     public static void main(String[] args) {
@@ -15,7 +18,19 @@ public class TestBrowser {
             driver = new FirefoxDriver();
             WebDriverManager.firefoxdriver().setup();
         }
-        driver.get("https://www.selenium.dev/");
+        driver.manage().window().maximize();
+//        driver.get("https://www.selenium.dev/");
+        driver.navigate().to("https://www.selenium.dev/");
+        sleep(5);
+        WebElement documentation = driver.findElement(By.xpath("(//a[@class='nav-link'])[2]"));
+        documentation.click();
+        System.out.println("This is the current URL: " + driver.getCurrentUrl());
+
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(pageTitle.contains("The Selenium Browser"));
+        driver.navigate().back();
+        sleep(5);
+        driver.navigate().forward();
         sleep(10);
         if(null != driver) {
             driver.quit();
